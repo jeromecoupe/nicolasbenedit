e254b94cdd6eaea3a7d220d474f3483416b78bdc
jérôme coupé <jercoupe@gmail.com>
2018-03-09 12:55:21 +0100

    initial commit
    Signed-off-by: jérôme coupé <jercoupe@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nicolas Benedit's website
+
+This is the website of Architect and Designer Nicolas Benedit. This website runs on [Jekyll](https://jekyllrb.com/)
+
+Despite being open sourced, all code and content remain copyright of Nicolas Benedit.
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const gulpChanged = require("gulp-changed");
+const gulpRename = require("gulp-rename");
+const gulpImagemin = require("gulp-imagemin");
+const gulpPostcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const browserSync = require("browser-sync").create();
+const childProcess = require("child_process");
+const del = require("del");
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "./_site/"
+    }
+  });
+}
+
+function browsersyncReload(done) {
+  browserSync.reload();
+  done();
+}
+
+function clean(done) {
+  return del(["./_site/assets/"]);
+  done();
+}
+
+function css() {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(gulpSass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(gulpRename({ suffix: ".min" }))
+    .pipe(gulpPostcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(browserSync.stream());
+}
+
+function jekyll(done) {
+  return childProcess
+    .spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" })
+    .on("close", done);
+}
+
+function images() {
+  return (
+    gulp
+      .src("./src/img/**/*")
+      //.pipe(gulpChanged("./_site/assets/img"))
+      .pipe(
+        gulpImagemin([
+          gulpImagemin.gifsicle({ interlaced: true }),
+          gulpImagemin.jpegtran({ progressive: true }),
+          gulpImagemin.optipng({ optimizationLevel: 5 }),
+          gulpImagemin.svgo({
+            plugins: [{ removeViewBox: false }]
+          })
+        ])
+      )
+      .pipe(gulp.dest("./_site/assets/img"))
+      .pipe(browserSync.stream())
+  );
+}
+
+gulp.task("images", images);
+gulp.task("watch", gulp.parallel(browsersync, watchFiles));
+gulp.task("build", gulp.series(clean, gulp.parallel(css, images, jekyll)));
+
+function watchFiles() {
+  gulp.watch("./src/scss/**/*", css);
+  gulp.watch(
+    ["./_includes/**/*", "./_layouts/**/*", "./_pages/**/*"],
+    gulp.series(jekyll, browsersyncReload)
+  );
+  gulp.watch("./src/img/**/*{.png,.jpg,.svg,.gif}", images);
+}
